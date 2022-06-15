2aeb03c62536e24cae2efe611c0771564d88847d
seomidia <suporte@seomidia.com.br>
2022-06-15 20:38:37 -0300

    ajustes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if($_SERVER['REQUEST_URI'] == '/admin/admissions'){
+        if(isset($_SERVER['REQUEST_URI']) && $_SERVER['REQUEST_URI'] == '/admin/admissions'){
             Voyager::addAction(\App\Actions\AmissionAction::class);
             Voyager::addAction(\App\Actions\PrintAction::class);
         }
