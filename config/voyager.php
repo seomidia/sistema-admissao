7f6a2ac9852692c213e561c330a3540c65167329
seomidia <suporte@seomidia.com.br>
2022-06-16 13:43:56 -0300

    ajustes

--- config/voyager.php
+++ config/voyager.php
@@ -182,13 +182,13 @@ return [
     |
     */
 
-    'primary_color' => '#22A7F0',
+    'primary_color' => '#776033',
 
     'show_dev_tips' => true, // Show development tip "How To Use:" in Menu and Settings
 
     // Here you can specify additional assets you would like to be included in the master.blade
     'additional_css' => [
-        //'css/custom.css',
+        'css/custom.css',
     ],
 
     'additional_js' => [
