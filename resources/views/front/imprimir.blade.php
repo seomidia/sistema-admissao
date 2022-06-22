9efce7f531c1de96f2e9745068717c274a9d54c9
seomidia <suporte@seomidia.com.br>
2022-06-22 06:05:48 -0300

    ajuste pdf

--- resources/views/front/imprimir.blade.php
+++ resources/views/front/imprimir.blade.php
@@ -126,8 +126,10 @@
             <td colspan="2"><a target="_blanck" href="{{url($files['pis']['url'])}}" class="btn btn-primary open"><label for="">PIS/PASEP </label> </a> </td>
             <td>{{$admission->pis}} @if($admission->primeiro_emprego =='sim') Primeiro emprego @endif</td>
             @else 
+            @if(isset($admission->primeiro_emprego))
             <td colspan="2"><label for="">PIS/PASEP </label></td>
             <td>@if($admission->primeiro_emprego =='sim') Primeiro emprego @endif</td>
+            @endif
             @endif
         </tr>
         <tr>
